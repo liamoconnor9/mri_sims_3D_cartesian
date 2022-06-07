fixed memory overflow problems

adjop/OptimizationContext.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import sys
 import dedalus.public as d3
+from dedalus.core.future import FutureField
 from mpi4py import MPI
 CW = MPI.COMM_WORLD
 import matplotlib.pyplot as plt
@@ -98,9 +99,15 @@ class OptimizationContext:
         self.backward_ic = OrderedDict()
         for forward_field in self.lagrangian_dict.keys():
             backward_field = self.lagrangian_dict[forward_field]
-            self.backward_ic[backward_field.name] = objectiveT.sym_diff(forward_field)
+            backic = objectiveT.sym_diff(forward_field)
+            if backic == 0:
+                continue
+            self.backward_ic[backward_field.name] = backic
 
     def reshape_soln(self, x):
+
+
+        # return x.reshape(self.domain.coeff_shape)[:]
         return x.reshape(self.domain.grid_shape(scales=1))[:]
 
     # For a given problem, these should be overwritten to add filehandlers, animations, metrics, etc.
@@ -122,32 +129,35 @@ class OptimizationContext:
     # Depreciated with scipy.optimization.minimize
     # calling this solves for the objective (forward) and the gradient (backward)
     def loop(self, x):
-        self.build_var_hotel() 
         f = self.loop_forward(x)
         f_prime = self.loop_backward(x)
         return f, f_prime
 
     def loop_forward(self, x):
 
+        self.build_var_hotel() 
         if (self.loop_index >= self.opt_iters):
             raise self.LoopIndexException({"message": "Achieved the proper number of loop index"})
-
+        
+        scales = self.opt_scales
+        layout = self.opt_layout
         self.x = x
-        self.ic['u'].change_scales(1)
-        self.old_x.change_scales(1)
-        self.new_x.change_scales(1)
-        # self.ic['u']['g'] = x.reshape((2,) + self.domain.grid_shape(scales=1))[:, self.slices[0], self.slices[1]]
+        self.ic['u'].change_scales(scales)
+        self.old_x.change_scales(scales)
+        self.new_x.change_scales(scales)
+        # self.ic['u'][layout] = x.reshape((2,) + self.domain.grid_shape(scales=1))[:, self.slices[0], self.slices[1]]
 
-        self.old_x['g'] = self.ic['u']['g'].copy()
-        self.ic['u']['g'] = self.reshape_soln(x)
-        self.new_x['g'] = self.ic['u']['g'].copy()
+        self.old_x[layout] = self.ic['u'][layout].copy()
+        
+        self.ic['u'][layout] = self.reshape_soln(x)
+        self.new_x[layout] = self.ic['u'][layout].copy()
 
         self.before_fullforward_solve()
         
         # Grab before fields are evolved (old state)
-        self.old_grad.change_scales(1)
-        self.backward_solver.state[0].change_scales(1)
-        self.old_grad['g'] = self.backward_solver.state[0]['g'].copy()
+        self.old_grad.change_scales(scales)
+        self.backward_solver.state[0].change_scales(scales)
+        self.old_grad[layout] = self.backward_solver.state[0][layout].copy()
 
         self.set_forward_ic()       
 
@@ -186,19 +196,21 @@ class OptimizationContext:
         self.backward_solver.evaluator.handlers.clear()
 
         # Evaluate after fields are evolved (new state)
-        self.backward_solver.state[0].change_scales(1)
-        self.backward_solver.state[0]['g']
+        scales = self.opt_scales
+        self.backward_solver.state[0].change_scales(scales)
+        layout = self.opt_layout
+        self.backward_solver.state[0][layout]
 
-        self.old_grad.change_scales(1)
-        self.new_grad.change_scales(1)
+        self.old_grad.change_scales(scales)
+        self.new_grad.change_scales(scales)
 
-        self.old_grad['g'] = self.new_grad['g'].copy()
-        self.new_grad['g'] = self.backward_solver.state[0]['g'].copy()
+        self.old_grad[layout] = self.new_grad[layout].copy()
+        self.new_grad[layout] = self.backward_solver.state[0][layout].copy()
 
         self.after_backward_solve()
         self.loop_index += 1
 
-        return self.gamma_init * self.backward_solver.state[0].allgather_data().flatten().copy()
+        return self.gamma_init * self.backward_solver.state[0].allgather_data(layout=layout).flatten().copy()
         
     # Set starting point for loop
     def set_forward_ic(self):
@@ -281,7 +293,7 @@ class OptimizationContext:
         # flip dictionary s.t. keys are backward var names and items are forward var names
         flipped_ld = dict((backward_var, forward_var) for forward_var, backward_var in self.lagrangian_dict.items())
         for backward_field in self.backward_solver.state:
-            if (backward_field.name in self.backward_ic.keys()):
+            if (backward_field.name in self.backward_ic.keys() and isinstance(self.backward_ic[backward_field.name], FutureField)):
                 backward_ic_field = self.backward_ic[backward_field.name].evaluate()
                 backward_field.change_scales(1)
                 backward_ic_field.change_scales(1)
@@ -292,6 +304,8 @@ class OptimizationContext:
         try:
             for t_ind in range(self.dt_per_cp):
                 for var in self.hotel.keys():
+                    if not var in self.backward_solver.problem.namespace.keys():
+                        continue
                     self.backward_solver.problem.namespace[var].change_scales(1)
                     self.backward_solver.problem.namespace[var]['g'] = self.hotel[var][-t_ind - 1]
                 self.backward_solver.step(-self.dt)

adjop/shear/shear_cg.py
@@ -54,14 +54,18 @@ logger.info(config.items('parameters'))
 # Parameters
 load_state = config.getboolean('parameters', 'load_state')
 basinhopping_iters = config.getint('parameters', 'basinhopping_iters')
+opt_cycles = config.getint('parameters', 'opt_cycles')
 opt_iters = config.getint('parameters', 'opt_iters')
 method = str(config.get('parameters', 'scipy_method'))
+opt_scales = config.getfloat('parameters', 'opt_scales')
+opt_layout = str(config.get('parameters', 'opt_layout'))
 
 num_cp = config.getint('parameters', 'num_cp')
 handler_loop_cadence = config.getint('parameters', 'handler_loop_cadence')
 add_handlers = config.getboolean('parameters', 'add_handlers')
 guide_coeff = config.getfloat('parameters', 'guide_coeff')
 omega_weight = config.getfloat('parameters', 'omega_weight')
+s_weight = config.getfloat('parameters', 's_weight')
 
 Lx = config.getfloat('parameters', 'Lx')
 Lz = config.getfloat('parameters', 'Lz')
@@ -91,8 +95,12 @@ backward_problem = BackwardShear.build_problem(domain, coords, Reynolds)
 
 # forward, and corresponding adjoint variables (fields)
 u = forward_problem.variables[0]
+s = forward_problem.variables[1]
+
 u_t = backward_problem.variables[0]
-lagrangian_dict = {u : u_t}
+s_t = backward_problem.variables[1]
+
+lagrangian_dict = {u : u_t, s : s_t}
 
 forward_solver = forward_problem.build_solver(d3.RK443)
 backward_solver = backward_problem.build_solver(d3.RK222)
@@ -118,7 +126,7 @@ with h5py.File(end_state_path) as f:
 if (load_state):
     restart_dir = path + '/' + write_suffix + '/checkpoints'
     checkpoint_names = [name for name in os.listdir(restart_dir) if 'loop' in name]
-    last_checkpoint = natsorted(checkpoint_names)[-1]
+    last_checkpoint = natsorted(checkpoint_names)[-2]
     restart_file = restart_dir + '/' + last_checkpoint + '/' + last_checkpoint + '_s1.h5'
     with h5py.File(restart_file) as f:
         opt.ic['u']['g'] = f['tasks/u'][-1, :, :][:, slices[0], slices[1]]
@@ -142,8 +150,8 @@ else:
 # set tracer initial condition
 opt.ic['s'] = dist.Field(name='s', bases=bases)
 opt.ic['s']['g'] = 1/2 + 1/2 * (np.tanh((z-0.5)/0.1) - np.tanh((z+0.5)/0.1))
-opt.backward_ic['s_t'] = dist.Field(name='s_t', bases=bases)
-opt.backward_ic['s_t']['g'] = 1/2 + 1/2 * (np.tanh((z-0.5)/0.1) - np.tanh((z+0.5)/0.1))
+# opt.backward_ic['s_t'] = dist.Field(name='s_t', bases=bases)
+# opt.backward_ic['s_t']['g'] = 1/2 + 1/2 * (np.tanh((z-0.5)/0.1) - np.tanh((z+0.5)/0.1))
 
 # Late time objective: objectiveT is minimized at t = T
 # w2 = d3.div(d3.skew(u))
@@ -157,15 +165,17 @@ Uz = U @ ez
 W = dx(Uz) - dz(Ux)
 # W2 = d3.div(d3.skew(U))
 
-# objectiveT = (w - W)**2
 objectiveT = d3.dot(u - U, u - U)
 opt.set_objectiveT(objectiveT)
+opt.objectiveT += omega_weight*(w - W)**2
+opt.objectiveT += s_weight*(s - S) * (s - S)
 
-# opt.backward_ic['u_t'] = -2.0*d3.skew(d3.grad((w - W)))
-# opt.backward_ic['u_t'] += 2*omega_weight * (ex * dz(w - W) - ez * dx(w - W))
+opt.backward_ic['s_t']= 2*s_weight*(s - S)
+opt.backward_ic['u_t'] += -2.0*omega_weight*d3.skew(d3.grad((w - W)))
 
 opt.metricsT['u_error'] = d3.dot(u - U, u - U)
 opt.metricsT['omega_error'] = (w - W)**2
+opt.metricsT['s_error'] = (s - S)**2
 opt.track_metrics()
 
 def check_status(x, f, accept):
@@ -175,12 +185,13 @@ def check_status(x, f, accept):
 def euler_descent(fun, x0, args, **kwargs):
     # gamma = 0.001
     # maxiter = kwargs['maxiter']
-    maxiter = 10
+    maxiter = opt_iters
     jac = kwargs['jac']
     for i in range(maxiter):
         f, gradf = opt.loop(x0)
         # gradf = jac(x0)
-        gamma = opt.compute_gamma(0.8)
+        # gamma = opt.compute_gamma(0.8)
+        gamma = 1e0
         x0 -= gamma * gradf
     logger.info('success')
     logger.info('maxiter = {}'.format(maxiter))
@@ -195,34 +206,49 @@ if (method == "euler"):
 from datetime import datetime
 startTime = datetime.now()
 
-opt.ic['u'].change_scales(1)
-opt.ic['u']['g']
-x0 = opt.ic['u'].allgather_data().flatten().copy()  # Initial guess.
+# Parameters to choose how in what dedalus layout scipy will optimize: e.g. optimize in grid space or coeff with some scale
+opt.opt_scales = opt_scales
+opt.opt_layout = opt_layout
+opt.dist_layout = dist.layout_references[opt_layout]
+opt.opt_slices = opt.dist_layout.slices(domain, scales=opt_scales)
+
+opt.ic['u'].change_scales(opt_scales)
+opt.ic['u'][opt_layout]
+
 
 options = {'maxiter' : opt_iters}
 minimizer_kwargs = {"method":method, "jac":True}
-try:
-    res1 = basinhopping(opt.loop, x0, T=1e-2, callback=check_status, minimizer_kwargs=minimizer_kwargs)
-    # res1 = basinhopping(opt.loop, x0, T=0.1, niter=basinhopping_iters, callback=check_status, minimizer_kwargs=minimizer_kwargs)
-    logger.info(res1)
-except opt.LoopIndexException as e:
-    details = e.args[0]
-    logger.info(details["message"])
-except opt.NanNormException as e:
-    details = e.args[0]
-    logger.info(details["message"])
-
-# try:
-#     res1 = minimize(opt.loop_forward, x0, jac=opt.loop_backward, options=options, callback=check_status, tol=1e-8, method=method)
-#     logger.info(res1)
-# except opt.LoopIndexException as e:
-#     details = e.args[0]
-#     logger.info(details["message"])
-# except opt.NanNormException as e:
-#     details = e.args[0]
-#     logger.info(details["message"])
-# except Exception as e:
-#     logger.info('Unknown exception occured: {}'.format(e))
+if (basinhopping_iters > 0):
+    try:
+        x0 = opt.ic['u'].allgather_data(layout=opt.dist_layout).flatten().copy()  # Initial guess.
+        res1 = basinhopping(opt.loop, x0, T=1e-2, callback=check_status, minimizer_kwargs=minimizer_kwargs)
+        # res1 = basinhopping(opt.loop, x0, T=0.1, niter=basinhopping_iters, callback=check_status, minimizer_kwargs=minimizer_kwargs)
+        logger.info(res1)
+    except opt.LoopIndexException as e:
+        details = e.args[0]
+        logger.info(details["message"])
+    except opt.NanNormException as e:
+        details = e.args[0]
+        logger.info(details["message"])
+else:
+    for cycle_ind in range(opt_cycles):
+        logger.info('Initiating optimization cycle {}'.format(cycle_ind))
+        try:
+            x0 = opt.ic['u'].allgather_data(layout=opt.dist_layout).flatten().copy()  # Initial guess.
+            res1 = minimize(opt.loop_forward, x0, jac=opt.loop_backward, options=options, tol=1e-8, method=method)
+            logger.info(res1)
+        except opt.LoopIndexException as e:
+            details = e.args[0]
+            logger.info(details["message"])
+        except opt.NanNormException as e:
+            details = e.args[0]
+            logger.info(details["message"])
+        except Exception as e:
+            logger.info('Unknown exception occured: {}'.format(e))
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            logger.info(exc_type, fname, exc_tb.tb_lineno)
+        opt.opt_iters += opt_iters
 
 logger.info('####################################################')
 logger.info('COMPLETED OPTIMIZATION RUN')